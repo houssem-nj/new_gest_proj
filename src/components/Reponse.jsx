f72186ab37520c904d102663db70c570d3c9f73e
houssemnj <123997285+houssemnj@users.noreply.github.com>
2024-05-26 17:26:17 +0100

    Update Reponse.jsx

--- src/components/Reponse.jsx
+++ src/components/Reponse.jsx
@@ -1,178 +1,3 @@
-// import React, { useState, useEffect, useRef } from "react";
-// import { SearchOutlined } from "@ant-design/icons";
-// import { Button, Input, Space, Table } from "antd";
-// import Highlighter from "react-highlight-words";
-// import axios from "axios";
-// import "./../App.css";
-
-// function Reponse() {
-//   const [details_refusees, setDetailsRefusees] = useState([]);
-//   const [searchText, setSearchText] = useState("");
-//   const [searchedColumn, setSearchedColumn] = useState("");
-//   const searchInput = useRef(null);
-//   const handleSearch = (selectedKeys, confirm, dataIndex) => {
-//     confirm();
-//     setSearchText(selectedKeys[0]);
-//     setSearchedColumn(dataIndex);
-//   };
-//   const handleReset = (clearFilters) => {
-//     clearFilters();
-//     setSearchText("");
-//   };
-//   const getColumnSearchProps = (dataIndex) => ({
-//     filterDropdown: ({
-//       setSelectedKeys,
-//       selectedKeys,
-//       confirm,
-//       clearFilters,
-//       close,
-//     }) => (
-//       <div
-//         style={{
-//           padding: 8,
-//         }}
-//         onKeyDown={(e) => e.stopPropagation()}
-//       >
-//         <Input
-//           ref={searchInput}
-//           placeholder={`Search ${dataIndex}`}
-//           value={selectedKeys[0]}
-//           onChange={(e) =>
-//             setSelectedKeys(e.target.value ? [e.target.value] : [])
-//           }
-//           onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-//           style={{
-//             marginBottom: 8,
-//             display: "block",
-//           }}
-//         />
-//         <Space>
-//           <Button
-//             type="primary"
-//             onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-//             icon={<SearchOutlined />}
-//             size="small"
-//             style={{
-//               width: 90,
-//             }}
-//           >
-//             Search
-//           </Button>
-//           <Button
-//             onClick={() => clearFilters && handleReset(clearFilters)}
-//             size="small"
-//             style={{
-//               width: 90,
-//             }}
-//           >
-//             Reset
-//           </Button>
-//           <Button
-//             type="link"
-//             size="small"
-//             onClick={() => {
-//               confirm({
-//                 closeDropdown: false,
-//               });
-//               setSearchText(selectedKeys[0]);
-//               setSearchedColumn(dataIndex);
-//             }}
-//           >
-//             Filter
-//           </Button>
-//           <Button
-//             type="link"
-//             size="small"
-//             onClick={() => {
-//               close();
-//             }}
-//           >
-//             close
-//           </Button>
-//         </Space>
-//       </div>
-//     ),
-//     filterIcon: (filtered) => (
-//       <SearchOutlined
-//         style={{
-//           color: filtered ? "#1677ff" : undefined,
-//         }}
-//       />
-//     ),
-//     onFilter: (value, record) =>
-//       record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-//     onFilterDropdownOpenChange: (visible) => {
-//       if (visible) {
-//         setTimeout(() => searchInput.current?.select(), 100);
-//       }
-//     },
-//     render: (text) =>
-//       searchedColumn === dataIndex ? (
-//         <Highlighter
-//           highlightStyle={{
-//             backgroundColor: "#ffc069",
-//             padding: 0,
-//           }}
-//           searchWords={[searchText]}
-//           autoEscape
-//           textToHighlight={text ? text.toString() : ""}
-//         />
-//       ) : (
-//         text
-//       ),
-//   });
-
-//   useEffect(() => {
-//     axios.get("http://:5002/traiter")
-//       .then((response) => {
-//         console.log("Réponse de /traiter:", response.data);
-//         setDetailsRefusees(response.data); // Met à jour l'état avec les données reçues
-//       })
-//       .catch((error) => {
-//         console.error("Erreur lors de la récupération des détails:", error);
-//       });
-//   }, []);
-
-
-
-//   const columns = [
-//     {
-//       title: "Date de demande",
-//       dataIndex: "Date de demande",
-//       key: "Date de demande",
-//       render: (text) => <p>{new Date(text).toLocaleString()}</p>,
-//     },
-//     {
-//       title: "ID Demande",
-//       dataIndex: "_id",
-//       key: "_id",
-//     },
-//     {
-//       title: "Chef Projet",
-//       dataIndex: "Chef de projet",
-//       key: "chef",
-//       ...getColumnSearchProps("Chef de projet"),
-//     },
-//     {
-//       title: "Etat",
-//       dataIndex: "etat",
-//     },
-
-//   ];
-
-//   // console.log(details_refusees);
-
-
-//   return (
-//     <div>
-//       <h2>Reponses des demandes refusées</h2>
-//       <Table columns={columns} dataSource={details_refusees} />
-//     </div>
-//   );
-// }
-
-// export default Reponse;
-
 import React, { useState, useEffect, useRef } from "react";
 import { SearchOutlined, BellOutlined } from "@ant-design/icons";
 import { Badge, Button, Input, Space, Table, notification } from "antd";
@@ -192,7 +17,7 @@ function Reponse() {
   useEffect(() => {
     const fetchNotifications = async () => {
       try {
-        const response = await axios.post("http://109.205.176.62:7010/gest_notif");
+        const response = await axios.post(`${anotherApiUrl}gest_notif`);
         if (response.data && response.data.length > 0) {
           // Trier les notifications par date décroissante
           const sortedNotifData = response.data.sort((a, b) => new Date(b.date) - new Date(a.date));
@@ -344,7 +169,7 @@ function Reponse() {
   });
 
   useEffect(() => {
-    axios.get("http://109.205.176.62:7010/traiter")
+    axios.get(`${anotherApiUrl}traiter`)
       .then((response) => {
         console.log("Réponse de /traiter:", response.data);
         setDetailsRefusees(response.data); // Met à jour l'état avec les données reçues
