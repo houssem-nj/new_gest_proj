b837a158db58000e2f4be6cc09656e6c98402633
houssemnj <123997285+houssemnj@users.noreply.github.com>
2024-05-28 21:05:53 +0100

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -37,7 +37,7 @@ const LoginForm = () => {
     const onFinish = async (values) => {
         try {
             setLoading(true);
-            const response = await axios.patch('http://109.205.176.62:6954/api/auth/login', values);
+            const response = await axios.patch(`${anotherApiUrl}api/auth/login`, values);
             const { token, role } = response.data;
 
             localStorage.setItem('authToken', token);
